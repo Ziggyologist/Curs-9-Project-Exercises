bc09f80b62d4e20fc9c24a1ebbca856da9e2a7d7
Teodora Vela <teodora11vela@gmail.com>
2025-01-27 16:15:02 +0200

    added cerinta for app 18

--- Exercitii/App18/Application18.cs
+++ Exercitii/App18/Application18.cs
@@ -11,7 +11,13 @@ namespace App18
     {
         public Application18()
         {
-            Cerinta = "Cerinta.";
+            Cerinta = "Realizati o aplicatie de tip consola in care sa indepliniti urmatoarele cerinte, in ordine:" +
+                "- Creati o clasa numita “Button” in care sa definiti un eveniment“Clicked”. " +
+                "Evenimentul va fi declansat printr-o metoda publica numita “SimulateClick”. Elementele trebuie sa fie parte din obiecte." +
+                "- Creati o clasa numita “Window”. Aceasta se va abona la evenimentul “Clicked” al oricarui buton adaugat in Constructor, iar listenerul va afisa un mesaj in consola." +
+                "- Extindeti clasa Button, adaugand ca argument la eveniment numele butonului. Modificati clasa Window astfel incat denumirea butonului apasat sa fie afisata." +
+                "- Implementati un mecanism de citire de la tastatura in bucla. La introducerea corecta a denumirii unui buton existent, acestuia ii va fi apelata metoda “SimulateClick”." +
+                "- Limitati elementele de cod astfel incat doar acele proprietati / metode dorite sa fie expuse..";
         }
 
         override public void Launch()
